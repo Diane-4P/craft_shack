94bce1c973a11dfcd542124e36da11d716efaf7c
Diane-4P <f39xy05z@students.codeinstitute.net>
2026-05-28 16:36:44 +0100

    Add subcategory

--- products/models.py
+++ products/models.py
@@ -14,10 +14,27 @@ class Category(models.Model):
     
     def get_friendly_name(self):
         return self.friendly_name
+
+
+class Subcategory(models.Model):
+    
+    class Meta:
+        verbose_name_plural = 'Subcategories'
+        
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
     
     
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    subcategory = models.ForeignKey('Subcategory', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category, Subcategory
 
 # Create your views here.
 def all_products(request):
@@ -9,20 +9,35 @@ def all_products(request):
     
     products = Product.objects.all()
     query = None
+    categories = None
+    subcategories = None
     
     if request.GET:
-        query = request.GET.get('q')
-        if not query:
-            messages.error(request, "You didn't enter any search criteria!")
-            return redirect(reverse('products'))
         
-        queries = Q(name__icontains=query) | Q(description__icontains=query)
-        products = products.filter(queries)
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
             
-    
+        if  'subcategory' in request.GET:
+            subcategories = request.GET['subcategory'].split(',')
+            products = products.filter(subcategory__name__in=subcategories)
+            subcategories = Subcategory.objects.filter(name__in=subcategories)
+        
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect('products')
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+            
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
+        'current_subcategories': subcategories,
     }
 
     return render(request, 'products/products.html', context)
