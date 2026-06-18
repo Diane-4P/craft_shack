4828cf6430ee6c5f6f17553de97c5517b8cb52e9
Diane-4P <f39xy05z@students.codeinstitute.net>
2026-06-18 17:54:35 +0100

    Completed webhook handler

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -76,8 +76,8 @@ form.addEventListener('submit', function(ev) {
                         line1: $.trim(form.street_address1.value),
                         line2: $.trim(form.street_address2.value),
                         city: $.trim(form.town_or_city.value),
-                        postal_code: $.trim(form.postcode.value),
                         country: $.trim(form.country.value),
+                        state: $.trim(form.county.value)
                     }
                 }
             },
@@ -90,6 +90,7 @@ form.addEventListener('submit', function(ev) {
                     city: $.trim(form.town_or_city.value),
                     postal_code: $.trim(form.postcode.value),
                     country: $.trim(form.country.value),
+                    state: $.trim(form.county.value)
                 }
             },
         }).then(function(result) {
