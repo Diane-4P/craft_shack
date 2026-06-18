1a295e0ea8b61b858be59f0e4915e3acad40c931
Diane-4P <f39xy05z@students.codeinstitute.net>
2026-06-18 12:59:50 +0100

    Added checkout form data caching in payment intent

--- checkout/models.py
+++ checkout/models.py
@@ -1,4 +1,4 @@
-import uuid
+import uuid # Used to generate a random, unique 32 character order number
 
 from django.db import models
 from django.db.models import Sum
@@ -23,6 +23,9 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    # For pid and save order in views from Google AI
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
