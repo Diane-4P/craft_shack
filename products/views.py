ec210f37e000b30837337b2854639cbe4853a823
Diane-4P <f39xy05z@students.codeinstitute.net>
2026-06-23 10:28:10 +0100

    Add product view, url and template

--- products/views.py
+++ products/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Product, Category, Subcategory
+from .forms import ProductForm
 
 # Create your views here.
 def all_products(request):
@@ -20,9 +22,8 @@ def all_products(request):
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
-            if sortkey == 'name':
-                sortkey = 'lower_name'
-                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'name':sortkey = 'lower_name'
+            products = products.annotate(lower_name=Lower('name'))
 
             if sortkey == 'category':
                 sortkey = 'category__name'
@@ -78,3 +79,13 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+    
+    return render(request, template, context)
