a00e0bd4a9059eb97d3cf73668433edcc15f31d4
Diane-4P <f39xy05z@students.codeinstitute.net>
2026-07-01 15:07:51 +0100

    Add footer with policies, urls, views and css

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
+    path('terms_and_conditions/', views.terms_and_conditions, name='terms_and_conditions'),
+    path('returns_and_refunds/', views.returns_and_refunds, name='returns_and_refunds'),
+    path('shipping_policy/', views.shipping_policy, name='shipping_policy'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,24 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def privacy_policy(request):
+    """ A view to return the privacy policy page """
+    
+    return render(request, 'home/privacy_policy.html')
+
+def terms_and_conditions(request):
+    """ A view to return the terms and conditions page """
+    
+    return render(request, 'home/terms_and_conditions.html')
+
+def returns_and_refunds(request):
+    """ A view to return the retuns and refunds page """
+    
+    return render(request, 'home/returns_and_refunds.html')
+
+def shipping_policy(request):
+    """ A view to return the shipping policy page """
+    
+    return render(request, 'home/shipping_policy.html')
